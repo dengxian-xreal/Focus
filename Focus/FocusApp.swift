ca25756948f3fa52eabc25dca6073b6dd06d1f9a
xd <dengxian0714@gmail.com>
2024-04-22 11:41:16 +0800

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by XREAL on 2024/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
